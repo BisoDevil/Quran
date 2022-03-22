77306eff25cca64eba75312143169b1c508f01db
Basem <bisodevil@gmail.com>
2022-03-22 02:09:03 +0200

    fix scroll in surah view

--- lib/app/modules/surah/views/surah_view.dart
+++ lib/app/modules/surah/views/surah_view.dart
@@ -10,90 +10,90 @@ class SurahView extends GetView<SurahController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 252, 252, 249),
-      body: NestedScrollView(
-        floatHeaderSlivers: true,
-        body: GetBuilder<SurahController>(
-          builder: (_) {
-            if (_.surah == null) {
-              return Center(
-                child: CircularProgressIndicator(
-                  strokeWidth: 1,
-                ),
-              );
-            }
-            return SingleChildScrollView(
-              padding: EdgeInsets.all(10),
-              controller: controller.scrollController,
-              child: Column(
-                children: [
-                  if (_.surah!.ayahs!.first.text
-                      .contains('بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ'))
-                    Text(
-                      'بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ',
-                      style: Get.textTheme.headline4!.copyWith(
-                        // height: 2.6,
-                        color: Colors.black,
-                        fontFamily: "Quran",
-                      ),
+      body: CustomScrollView(
+        controller: controller.scrollController,
+        slivers: [
+          SliverAppBar(
+            title: Obx(
+              () => Text(
+                controller.surahName.value,
+              ),
+            ),
+            backgroundColor: Color.fromARGB(255, 252, 252, 249),
+            floating: true,
+          ),
+          SliverToBoxAdapter(
+            child: GetBuilder<SurahController>(
+              builder: (_) {
+                if (_.surah == null) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      strokeWidth: 1,
                     ),
-                  RichText(
-                    // textDirection: TextDirection.rtl,
-                    text: TextSpan(
-                      text: "",
-                      style: Get.textTheme.headline6!.copyWith(
-                        height: 2.6,
-                        fontWeight: FontWeight.normal,
-                        fontFamily: "Quran",
-                      ),
-                      children: _.surah!.ayahs!.map((item) {
-                        if (item.text.contains(
-                            'بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ')) {
-                          return TextSpan(
-                            text:
-                                " ${item.text.replaceFirst('بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ', '')} ",
-                            style: Get.textTheme.headline5!.copyWith(
-                              height: 2.6,
-                              fontFamily: "Quran",
-                            ),
-                          );
-                        }
+                  );
+                }
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      if (_.surah!.ayahs!.first.text
+                          .contains('بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ'))
+                        Text(
+                          'بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ',
+                          style: Get.textTheme.headline4!.copyWith(
+                            // height: 2.6,
+                            color: Colors.black,
+                            fontFamily: "Quran",
+                          ),
+                        ),
+                      RichText(
+                        textDirection: TextDirection.rtl,
+                        text: TextSpan(
+                          text: "",
+                          style: Get.textTheme.headline6!.copyWith(
+                            height: 2.6,
+                            fontWeight: FontWeight.normal,
+                            fontFamily: "Quran",
+                          ),
+                          children: _.surah!.ayahs!.map((item) {
+                            if (item.text.contains(
+                                'بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ')) {
+                              return TextSpan(
+                                text:
+                                    " ${item.text.replaceFirst('بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ', '')} ",
+                                style: Get.textTheme.headline5!.copyWith(
+                                  height: 2.6,
+                                  fontFamily: "Quran",
+                                ),
+                              );
+                            }
 
-                        return TextSpan(
-                          text: " ${item.text} ",
-                          recognizer: LongPressGestureRecognizer()
-                            ..onLongPress = () {
-                              controller.ayahAction(
-                                  item, _.scrollController.offset);
-                            },
-                          children: [
-                            WidgetSpan(
-                              child:
-                                  AyahNumberWidget(number: item.numberInSurah),
-                              alignment: PlaceholderAlignment.bottom,
-                            ),
-                          ],
-                        );
-                      }).toList(),
-                    ),
-                    textAlign: TextAlign.justify,
+                            return TextSpan(
+                              text: " ${item.text} ",
+                              recognizer: LongPressGestureRecognizer()
+                                ..onLongPress = () {
+                                  controller.ayahAction(
+                                      item, _.scrollController.offset);
+                                },
+                              children: [
+                                WidgetSpan(
+                                  child: AyahNumberWidget(
+                                      number: item.numberInSurah),
+                                  alignment: PlaceholderAlignment.bottom,
+                                ),
+                              ],
+                            );
+                          }).toList(),
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            );
-          },
-        ),
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              title: Obx(
-                () => Text(
-                  controller.surahName.value,
-                ),
-              ),
-              floating: true,
-            )
-          ];
-        },
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
